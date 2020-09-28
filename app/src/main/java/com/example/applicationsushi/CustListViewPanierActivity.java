ad869bf92a8a8c57511f9da3f564dc18fac0907d
Zaid Lamarti <zaid.lamarti.21@gmail.com>
2020-09-28 18:07:01 +0000

    .

--- app/src/main/java/com/example/applicationsushi/CustListViewPanierActivity.java
+++ app/src/main/java/com/example/applicationsushi/CustListViewPanierActivity.java
@@ -1,4 +1,181 @@
 package com.example.applicationsushi;
 
-public class CustListViewPanierActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class CustListViewPanierActivity extends AppCompatActivity {
+
+    int idPlat[];
+    String nom[] ;
+    String description[] ;
+    int id ;
+    int mImage[] ;
+    Double prix[] ;
+    Double note[] ;
+    String urlImages[];
+    Button Logout;
+    ImageView icon ;
+
+    CardView cardViewAcceuil ;
+    CardView cardViewCategories ;
+    CardView cardViewPanier ;
+    CardView cardViewRestaurant ;
+
+    ListView listView;
+
+    BufferedInputStream is ;
+    String line=null ;
+    String result=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cust_list_view_panier);
+
+        Logout = findViewById(R.id.buttonView);
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustListViewPanierActivity.this, LoginActivity.class));
+            }
+        });
+
+        cardViewAcceuil = findViewById(R.id.cardView1);
+        cardViewAcceuil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CustListViewPanierActivity.this , CustMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardViewCategories = findViewById(R.id.cardView2);
+        cardViewCategories.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustListViewPanierActivity.this, CustCategoriesActivity.class));
+            }
+        });
+
+        cardViewPanier = findViewById(R.id.cardView3) ;
+        cardViewPanier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustListViewPanierActivity.this, CustListViewPanierActivity.class));
+            }
+        });
+
+        cardViewRestaurant = findViewById(R.id.cardView4);
+        cardViewRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustListViewPanierActivity.this, CustListViewRestaurantActivity.class));
+            }
+        });
+
+        listView = findViewById(R.id.listview);
+        StrictMode.setThreadPolicy((new StrictMode.ThreadPolicy.Builder().permitNetwork().build()));
+        collectData();
+        final CustomListViewPanier customListViewPanier = new CustomListViewPanier(this , id , nom , description , urlImages);
+        listView.setAdapter(customListViewPanier);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long iid) {
+                Intent i = new Intent(CustListViewPanierActivity.this , CustInfoPlatActivity.class);
+
+                i.putExtra("id" , id) ;
+
+                i.putExtra("idPlat" , idPlat[position]);
+
+                i.putExtra("nom" , nom[position] );
+                i.putExtra("description" , description[position]);
+                i.putExtra("prix" , prix[position]);
+                i.putExtra("note" , note[position]);
+                i.putExtra("imgUrl" , urlImages[position]);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void collectData(){
+        try {
+            URL url = new URL("https://miamsushi.000webhostapp.com/connection/dpPlat.php/");
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            is=new BufferedInputStream(con.getInputStream());
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+
+            while ((line=br.readLine())!=null){
+                sb.append(line+"\n");
+            }
+
+            is.close();
+            result = sb.toString();
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+
+        try {
+
+            JSONArray js = new JSONArray(result);
+            JSONObject jo = null;
+
+            id=new Integer(js.length());
+
+            idPlat= new int[js.length()];
+
+            nom=new String[js.length()];
+            description=new String[js.length()];
+            prix=new Double[js.length()];
+            note=new Double[js.length()];
+            urlImages=new String[js.length()];
+
+            for (int i = 0 ; i<=js.length();i++){
+                jo = js.getJSONObject(i);
+
+                id=jo.getInt("idPlat");
+
+                idPlat[i]=jo.getInt("idPlat");
+
+                nom[i]=jo.getString("nom");
+                description[i]=jo.getString("description");
+                note[i] = jo.getDouble("note");
+                prix[i] = jo.getDouble("prix");
+                urlImages[i] = jo.getString("imageUrl");
+            }
+
+        } catch (Exception e ){
+            e.printStackTrace();
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/applicationsushi/CustomListViewPanier.java
@@ -0,0 +1,180 @@
+package com.example.applicationsushi;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class CustomListViewPanier extends ArrayAdapter<String> {
+
+    private String[] nom ;
+    private String[] description ;
+    private String[] imagePath ;
+    private int id ;
+    private Activity context ;
+
+    Bitmap bitmap;
+
+    public CustomListViewPanier(Activity context , int id , String[] nom , String[] description , String[] imagePath){
+        super(context,R.layout.row_panier,nom);
+
+        this.context=context;
+        this.nom=nom;
+        this.id=id;
+        this.description=description;
+        this.imagePath=imagePath;
+    }
+
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View r = convertView;
+        ViewHolder viewHolder;
+        ImageView info , buy ;
+
+        //dp data
+        if(r==null){
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            r=layoutInflater.inflate(R.layout.row , null , true);
+            viewHolder=new ViewHolder(r);
+            r.setTag(viewHolder);
+
+        } else {
+
+            viewHolder=(ViewHolder)r.getTag();
+
+        }
+        viewHolder.dpNom.setText(nom[position]);
+        viewHolder.dpDescription.setText(description[position]);
+        new GetImageFromUrl(viewHolder.imagePlat).execute(imagePath[position]);
+
+        //image clickable
+        info = (ImageView)r.findViewById(R.id.imageView2);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context , LoginActivity.class);
+                context.startActivity(i);
+            }
+        });
+
+
+        buy = (ImageView)r.findViewById(R.id.imageView3);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String ss ;
+                int idd ;
+
+                ss = LoginActivity.S ;
+                idd = id ;
+
+                Retrofit retrofit = new Retrofit.Builder().baseUrl("https://miamsushi.000webhostapp.com/connection/addPanier.php/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                RequestInterface request = retrofit.create(RequestInterface.class);
+                Call<JsonResponse> call = request.addPanier(ss,idd);
+                call.enqueue(new Callback<JsonResponse>() {
+                    @Override
+                    public void onResponse(Call<JsonResponse> call, Response<JsonResponse> response) {
+                        if(response.code()==200){
+                            JsonResponse jsonResponse = response.body();
+                            Toast.makeText(context.getApplicationContext(),jsonResponse.getResponse().toString(), Toast.LENGTH_SHORT).show();
+                            if(jsonResponse.getResponse().equals("Added Successfully")){
+                                Toast.makeText(context.getApplicationContext(),"Item added to wish list", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(context.getApplicationContext(), String.valueOf(response.code()),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonResponse> call, Throwable t) {
+                        Toast.makeText(context.getApplicationContext(),"Erreur",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        return r;
+    }
+
+
+    public class ViewHolder{
+
+        TextView dpNom;
+        TextView dpDescription;
+        ImageView bInfo;
+        ImageView bBuy;
+        ImageView imagePlat ;
+
+        ViewHolder(View v){
+            dpNom=(TextView)v.findViewById(R.id.textView);
+            dpDescription=(TextView)v.findViewById(R.id.textView2);
+            bInfo=(ImageView) v.findViewById(R.id.imageView2);
+            bBuy=(ImageView) v.findViewById(R.id.imageView3);
+            imagePlat=(ImageView) v.findViewById(R.id.imageView);
+
+        }
+    }
+
+    public class GetImageFromUrl extends AsyncTask<String,Void, Bitmap>{
+
+        ImageView imageView;
+
+        public GetImageFromUrl(ImageView imageView){
+            this.imageView=imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... url) {
+            String urldisplay=url[0];
+            bitmap=null;
+
+
+            try {
+
+                InputStream inputStream = new java.net.URL(urldisplay).openStream();
+                bitmap= BitmapFactory.decodeStream(inputStream);
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+            return bitmap;
+        }
+
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+}
