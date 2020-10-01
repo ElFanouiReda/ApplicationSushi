ca20b84164220181e0c165155c03692bd151309a
unknown <fanoui.reda@gmail.com>
2020-10-01 10:58:55 +0100

    Correction code

--- app/src/main/java/com/example/applicationsushi/CustomListViewPlat.java
+++ app/src/main/java/com/example/applicationsushi/CustomListViewPlat.java
@@ -30,22 +30,22 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class CustomListViewPlat extends ArrayAdapter<String> {
 
 
-    private String[] nom ;
-    private String[] description ;
-    private String[] imagePath ;
-    private int[] id ;
-    private Activity context ;
+    private String[] nom;
+    private String[] description;
+    private String[] imagePath;
+    private int[] id;
+    private Activity context;
 
     Bitmap bitmap;
 
-    public CustomListViewPlat(Activity context , int[] id , String[] nom , String[] description , String[] imagePath){
-        super(context,R.layout.row,nom);
+    public CustomListViewPlat(Activity context, int[] id, String[] nom, String[] description, String[] imagePath) {
+        super(context, R.layout.row, nom);
 
-        this.context=context;
-        this.nom=nom;
-        this.id=id;
-        this.description=description;
-        this.imagePath=imagePath;
+        this.context = context;
+        this.nom = nom;
+        this.id = id;
+        this.description = description;
+        this.imagePath = imagePath;
     }
 
 
@@ -54,116 +54,102 @@ public class CustomListViewPlat extends ArrayAdapter<String> {
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View r = convertView;
         ViewHolder viewHolder;
-        ImageView info , buy ;
+        ImageView info, buy;
 
         //dp data
-        if(r==null){
+        if (r == null) {
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            r=layoutInflater.inflate(R.layout.row , null , true);
-            viewHolder=new ViewHolder(r);
+            r = layoutInflater.inflate(R.layout.row, null, true);
+            viewHolder = new ViewHolder(r);
             r.setTag(viewHolder);
 
         } else {
 
-            viewHolder=(ViewHolder)r.getTag();
+            viewHolder = (ViewHolder) r.getTag();
 
         }
-            viewHolder.dpNom.setText(nom[position]);
-            viewHolder.dpDescription.setText(description[position]);
-            new GetImageFromUrl(viewHolder.imagePlat).execute(imagePath[position]);
+        viewHolder.dpNom.setText(nom[position]);
+        viewHolder.dpDescription.setText(description[position]);
+        new GetImageFromUrl(viewHolder.imagePlat).execute(imagePath[position]);
 
-            //image clickable
-       // info = (ImageView)r.findViewById(R.id.imageView2);
+        //image clickable
+        // info = (ImageView)r.findViewById(R.id.imageView2);
         //info.setOnClickListener(new View.OnClickListener() {
-      //      @Override
-      //      public void onClick(View v) {
-       //         Intent i = new Intent(context , LoginActivity.class);
+        //      @Override
+        //      public void onClick(View v) {
+        //         Intent i = new Intent(context , LoginActivity.class);
         //        context.startActivity(i);
-       //     }
+        //     }
         //});
 
 
-        buy = (ImageView)r.findViewById(R.id.imageView3);
+        buy = (ImageView) r.findViewById(R.id.imageView3);
         buy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String ss ;
-                int[] idd ;
+                String ss;
+                int[] idd;
 
-                ss = LoginActivity.S ;
-                idd = id ;
+                ss = LoginActivity.S;
+                idd = id;
 
                 Retrofit retrofit = new Retrofit.Builder().baseUrl("https://miamsushi.000webhostapp.com/connection/addPanier.php/")
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
                 RequestInterface request = retrofit.create(RequestInterface.class);
-                Call<JsonResponse> call = request.addPanier(ss,idd);
+                Call<JsonResponse> call = request.addPanier(ss, idd);
                 call.enqueue(new Callback<JsonResponse>() {
                     @Override
                     public void onResponse(Call<JsonResponse> call, Response<JsonResponse> response) {
-                        if(response.code()==200){
+                        if (response.code() == 200) {
                             JsonResponse jsonResponse = response.body();
-                            Toast.makeText(context.getApplicationContext(),jsonResponse.getResponse().toString(), Toast.LENGTH_SHORT).show();
-                            if(jsonResponse.getResponse().equals("Added Successfully")){
-                                Toast.makeText(context.getApplicationContext(),"Item added to wish list", Toast.LENGTH_SHORT).show();
-                            }
+                            Toast.makeText(context.getApplicationContext(), "jsonResponse.getResponse().toString()", Toast.LENGTH_SHORT).show();
+
+                            Toast.makeText(context.getApplicationContext(), "Item added to wish list", Toast.LENGTH_SHORT).show();
+
                         }
-                        else{
-                            Toast.makeText(context.getApplicationContext(), String.valueOf(response.code()),Toast.LENGTH_SHORT).show();
-                        }
+
                     }
 
                     @Override
                     public void onFailure(Call<JsonResponse> call, Throwable t) {
-                        Toast.makeText(context.getApplicationContext(),"Erreur",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context.getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
                     }
                 });
+
             }
+
+            ;
+
+
         });
 
         return r;
+
     }
 
 
-    public class ViewHolder{
-
-        TextView dpNom;
-        TextView dpDescription;
-        ImageView bInfo;
-        ImageView bBuy;
-        ImageView imagePlat ;
-
-        ViewHolder(View v){
-            dpNom=(TextView)v.findViewById(R.id.textView);
-            dpDescription=(TextView)v.findViewById(R.id.textView2);
-             bInfo=(ImageView) v.findViewById(R.id.imageView2);
-            bBuy=(ImageView) v.findViewById(R.id.imageView3);
-            imagePlat=(ImageView) v.findViewById(R.id.imageView);
-
-        }
-    }
-
-    public class GetImageFromUrl extends AsyncTask<String,Void, Bitmap>{
+    public class GetImageFromUrl extends AsyncTask<String, Void, Bitmap> {
 
         ImageView imageView;
 
-        public GetImageFromUrl(ImageView imageView){
-            this.imageView=imageView;
+        public GetImageFromUrl(ImageView imageView) {
+            this.imageView = imageView;
         }
 
         @Override
         protected Bitmap doInBackground(String... url) {
-            String urldisplay=url[0];
-            bitmap=null;
+            String urldisplay = url[0];
+            bitmap = null;
 
 
             try {
 
                 InputStream inputStream = new java.net.URL(urldisplay).openStream();
-                bitmap= BitmapFactory.decodeStream(inputStream);
+                bitmap = BitmapFactory.decodeStream(inputStream);
 
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
@@ -179,6 +165,22 @@ public class CustomListViewPlat extends ArrayAdapter<String> {
         }
     }
 
+    public class ViewHolder {
 
+        TextView dpNom;
+        TextView dpDescription;
+        ImageView bInfo;
+        ImageView bBuy;
+        ImageView imagePlat;
+
+        ViewHolder(View v) {
+            dpNom = (TextView) v.findViewById(R.id.textView);
+            dpDescription = (TextView) v.findViewById(R.id.textView2);
+            bInfo = (ImageView) v.findViewById(R.id.imageView2);
+            bBuy = (ImageView) v.findViewById(R.id.imageView3);
+            imagePlat = (ImageView) v.findViewById(R.id.imageView);
+
+        }
+    }
 
 }
